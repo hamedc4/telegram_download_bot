e0b66e906e95af4d883e384d9ae39926d618c825
hamedc4 <101958593+hamedc4@users.noreply.github.com>
2025-09-29 03:34:42 +0330

    Add files via upload

---
+++ env_file.sh
@@ -0,0 +1,16 @@
+# تنظیمات ربات تلگرام
+BOT_TOKEN=YOUR_BOT_TOKEN_HERE
+
+# تنظیمات دانلود
+MAX_FILE_SIZE=52428800  # 50MB
+DOWNLOAD_DIR=downloads
+CLEANUP_INTERVAL=3600   # 1 hour
+
+# تنظیمات لاگ
+LOG_LEVEL=INFO
+LOG_FILE=bot.log
+
+# تنظیمات پروکسی (اختیاری)
+# PROXY_URL=http://proxy:port
+# PROXY_USERNAME=username
+# PROXY_PASSWORD=password
